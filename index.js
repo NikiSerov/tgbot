c295704b6745d4e0e1d4849c8866d096ff54f33d
nikitaserov <nikiserov1997js@gmail.com>
2023-07-30 16:43:54 +0400

    added theme styles

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 Telegram.WebApp.ready();
+Telegram.WebApp.expand();
+
 Telegram.WebApp.MainButton.setText('Мой заказ').show().onClick(function () {
     const data = "DOROWA BANDIT"
     Telegram.WebApp.sendData(data);
     Telegram.WebApp.close();
 });
+
+
+Telegram.WebApp.BackButton.isVisible(true);
+Telegram.WebApp.BackButton.show();
